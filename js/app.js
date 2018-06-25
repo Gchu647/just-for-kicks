56fbe384eca6d43f8877cfad2272e55e452a80ea
George <quicksilver-ga@hotmail.com>
2018-06-24 20:27:40 -1000

    Finished

--- js/app.js
+++ js/app.js
@@ -8,7 +8,6 @@ name1.addEventListener("mouseover", function () { displayDescrip(jordan2Des); })
 name1.addEventListener("mouseout", function () { displayDescrip(jordan2Des); });
 
 function displayDescrip(des) {
-    console.log(des);
     if (!des.style.display || des.style.display === "none") {
         des.style.display = "block";
     } else {
@@ -74,13 +73,74 @@ function increasePrice() {
 
 
 //6. Add an event to the minus icon that will decrement the price of the Air Jordan V shoe each time the icon is clicked on.
+var decreaseIcon = decrease.children[0];
+var airJordan6Price = decrease.parentElement;
+decreaseIcon.addEventListener("click", decreasePrice);
+
+function decreasePrice() {
+    var num = parseInt(airJordan6Price.innerText) - 1;
+    airJordan6Price.innerHTML = num + "<span id='decrease'><i class='far fa-minus-square'></i> </span>";
+    decreaseIcon = decrease.children[0];
+    decreaseIcon.addEventListener("click", decreasePrice);
+}
 
 
-//7. Add an event to the Air Jordan XI shoe that will show another colorway for that shoe after hovering over the image. 
+//7. Add an event to the Air Jordan XI shoe that will show another colorway for that shoe after hovering over the image.
+// https://www.vladtv.com/images/size_fs/video-242119.jpg
+var block3Class1 = document.getElementsByClassName("block3 col-sm-4")[1];
+var firstColor = true;
+block3Class1.children[0].addEventListener("mouseover", colorWay);
+block3Class1.children[0].addEventListener("mouseout", colorWay);
+
+function colorWay() {
+    if (firstColor === true) {
+        this.src = "https://www.vladtv.com/images/size_fs/video-242119.jpg";
+        firstColor = false;
+    } else {
+        this.src = "https://www.sneakerlegends.com/images/stories/virtuemart/product/378037623%20378037623%20378037623%20378037623.jpg";
+        firstColor = true;
+    }
+}
 
 
 //8. Add an event to the Air Jordan XII shoe that will increase the image by 50% after clicking on the image.
 
+var block1class2 = document.getElementsByClassName("block1 col-sm-4")[2];
+block1class2.children[0].addEventListener("click", imgLarge);
+
+function imgLarge() {
+    this.style.height = "500px";
+    this.style.width = "500px";
+}
+
 //9. Add events to both the plus and minus icon that will add or decrease the price each time the respective icon is clicked on.
+var airJordan14Price = oneUp.parentElement;
+var oneUpIcon = oneUp.children[0];
+var oneDownIcon = oneDown.children[0];
+oneUpIcon.addEventListener("click", function () { testFunction(true) });
+oneDownIcon.addEventListener("click", function () { testFunction(false) });
+
+function testFunction(checkIncrease) {
+    var num = parseInt(airJordan14Price.innerText);
+
+    if (checkIncrease === true) {
+        num += 1;
+    } else {
+        num -= 1;
+    }
+
+    airJordan14Price.innerHTML = num + "<span id = 'oneUp'> <i class='far fa-plus-square'></i></span ><span id='oneDown'><i class='far fa-minus-square'></i></span>";
+
+    var oneUpIcon = oneUp.children[0];
+    var oneDownIcon = oneDown.children[0];
+    oneUpIcon.addEventListener("click", function () { testFunction(true) });
+    oneDownIcon.addEventListener("click", function () { testFunction(false) });
+}
 
 //10. Add an event to the Air Jordan XXVIII shoe that will show the famous 'crying Michael Jordan' meme after clicking on the image.
+var airJordan28Img = document.getElementsByClassName("block3 col-sm-4")[2].children[0];
+airJordan28Img.addEventListener("click", jordanMeme);
+
+function jordanMeme() {
+    this.src = "https://upload.wikimedia.org/wikipedia/en/thumb/c/c7/Michael_Jordan_crying.jpg/220px-Michael_Jordan_crying.jpg";
+}
